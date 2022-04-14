cc0bfe506ebc89b2d76c086e950991ffc59980bc
呉真 <kuretru@gmail.com>
2022-04-14 16:03:45 +0800

    重构(API)：提取OAuth2Client通用逻辑

--- Libra-Fortune-Api/libra-fortune-api/src/main/java/com/kuretru/web/libra/Application.java
+++ Libra-Fortune-Api/libra-fortune-api/src/main/java/com/kuretru/web/libra/Application.java
@@ -1,9 +1,16 @@
 package com.kuretru.web.libra;
 
+import com.kuretru.microservices.oauth2.client.system.EnableOAuth2System;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author 呉真(kuretru) <kuretru@gmail.com>
+ */
 @SpringBootApplication
+@EnableOAuth2System
+@MapperScan("com.kuretru.web.libra.mapper")
 public class Application {
 
     public static void main(String[] args) {
