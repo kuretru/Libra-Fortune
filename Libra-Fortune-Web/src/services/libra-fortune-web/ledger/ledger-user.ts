b203de7f8a1ed050745c38e761c4f94f31e1d2b0
呉真 <kuretru@gmail.com>
2023-04-17 09:12:05 +0800

    删除(Web)：旧前端项目

--- Libra-Fortune-Web/src/services/libra-fortune-web/ledger/ledger-user.ts
+++
@@ -1,9 +0,0 @@
-import BaseService from '../base-service';
-
-class LedgerUserService extends BaseService<API.Ledger.LedgerUserDTO, API.Ledger.LedgerUserQuery> {
-  constructor(ledgerId: string) {
-    super(`/ledgers/${ledgerId}/users`);
-  }
-}
-
-export default LedgerUserService;
--- Libra-Fortune-Web/src/services/libra-fortune-web/ledger/ledger.ts
+++
@@ -1,9 +0,0 @@
-import BaseService from '../base-service';
-
-class LedgerService extends BaseService<API.Ledger.LedgerDTO, API.Ledger.LedgerQuery> {
-  constructor() {
-    super('/ledgers');
-  }
-}
-
-export default LedgerService;
