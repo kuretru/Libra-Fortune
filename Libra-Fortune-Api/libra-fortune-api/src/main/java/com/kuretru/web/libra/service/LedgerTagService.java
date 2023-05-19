68df5e94f0637426c296c7f494b9a49fdf379708
呉真 <kuretru@gmail.com>
2023-05-19 11:48:20 +0800

    新增(API)：完善账本标签逻辑

--- Libra-Fortune-Api/libra-fortune-api/src/main/java/com/kuretru/web/libra/service/LedgerTagService.java
+++ Libra-Fortune-Api/libra-fortune-api/src/main/java/com/kuretru/web/libra/service/LedgerTagService.java
@@ -4,9 +4,19 @@ import com.kuretru.microservices.web.service.BaseService;
 import com.kuretru.web.libra.entity.query.LedgerTagQuery;
 import com.kuretru.web.libra.entity.transfer.LedgerTagDTO;
 
+import java.util.Map;
+import java.util.UUID;
+
 /**
  * @author 呉真(kuretru) <kuretru@gmail.com>
  */
 public interface LedgerTagService extends BaseService<LedgerTagDTO, LedgerTagQuery> {
+
+    /**
+     * 列出我的账本标签
+     *
+     * @return 账本标签Map
+     */
+    Map<UUID, LedgerTagDTO> listMyLedgerTags();
 
 }
--- Libra-Fortune-Api/libra-fortune-api/src/main/java/com/kuretru/web/libra/service/impl/LedgerTagServiceImpl.java
+++ Libra-Fortune-Api/libra-fortune-api/src/main/java/com/kuretru/web/libra/service/impl/LedgerTagServiceImpl.java
@@ -2,8 +2,11 @@ package com.kuretru.web.libra.service.impl;
 
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.kuretru.microservices.authentication.context.AccessTokenContext;
+import com.kuretru.microservices.common.utils.HashMapUtils;
 import com.kuretru.microservices.web.constant.code.ServiceErrorCodes;
 import com.kuretru.microservices.web.constant.code.UserErrorCodes;
+import com.kuretru.microservices.web.entity.PaginationQuery;
+import com.kuretru.microservices.web.entity.PaginationResponse;
 import com.kuretru.microservices.web.exception.ServiceException;
 import com.kuretru.microservices.web.service.impl.BaseServiceImpl;
 import com.kuretru.web.libra.entity.data.LedgerTagDO;
@@ -15,6 +18,9 @@ import com.kuretru.web.libra.service.LedgerTagService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.UUID;
 
 /**
@@ -31,6 +37,24 @@ public class LedgerTagServiceImpl
     }
 
     @Override
+    public Map<UUID, LedgerTagDTO> listMyLedgerTags() {
+        List<LedgerTagDTO> records = list();
+        Map<UUID, LedgerTagDTO> result = new HashMap<>(HashMapUtils.initialCapacity(records.size()));
+        records.forEach(record -> result.put(record.getId(), record));
+        return result;
+    }
+
+    @Override
+    public List<LedgerTagDTO> list() {
+        throw new ServiceException(UserErrorCodes.MISSING_REQUIRED_PARAMETERS, "账本标签必须关联用户ID查询");
+    }
+
+    @Override
+    public PaginationResponse<LedgerTagDTO> list(PaginationQuery paginationQuery) {
+        throw new ServiceException(UserErrorCodes.MISSING_REQUIRED_PARAMETERS, "账本标签必须关联用户ID查询");
+    }
+
+    @Override
     public LedgerTagDTO save(LedgerTagDTO record) throws ServiceException {
         record.setUserId(AccessTokenContext.getUserId());
         return super.save(record);
@@ -39,20 +63,19 @@ public class LedgerTagServiceImpl
     @Override
     public LedgerTagDTO update(LedgerTagDTO record) throws ServiceException {
         record.setUserId(AccessTokenContext.getUserId());
-        LedgerTagDTO old = get(record.getId());
+
+        LedgerTagDO old = getDO(record.getId());
         if (old == null) {
             throw new ServiceException(UserErrorCodes.REQUEST_PARAMETER_ERROR, "指定记录不存在");
-        } else if (!old.getUserId().equals(record.getUserId())) {
-            // 不能把别人的记录修改为自己的
+        }
+
+        // 不能把别人的记录修改为自己的
+        boolean notMyRecord = !UUID.fromString(old.getUserId()).equals(record.getUserId());
+        if (notMyRecord) {
             throw new ServiceException(UserErrorCodes.REQUEST_PARAMETER_ERROR, "无法修改非自己的账本标签");
         }
 
         return super.update(record);
-    }
-
-    @Override
-    public void remove(UUID uuid) throws ServiceException {
-        throw new ServiceException(ServiceErrorCodes.SYSTEM_EXECUTION_ERROR, "尚未实现");
     }
 
     @Override
@@ -63,10 +86,16 @@ public class LedgerTagServiceImpl
     }
 
     @Override
-    protected void verifyDO(LedgerTagDO record) throws ServiceException {
-        if (!UUID.fromString(record.getUserId()).equals(AccessTokenContext.getUserId())) {
+    protected void verifyCanGet(LedgerTagDO record) throws ServiceException {
+        boolean notMe = !UUID.fromString(record.getUserId()).equals(AccessTokenContext.getUserId());
+        if (notMe) {
             throw new ServiceException(UserErrorCodes.ACCESS_UNAUTHORIZED, "只能查询自己的账本标签");
         }
+    }
+
+    @Override
+    protected void verifyCanRemove(LedgerTagDO record) throws ServiceException {
+        throw new ServiceException(ServiceErrorCodes.SYSTEM_NOT_IMPLEMENTED, "尚未实现");
     }
 
     @Override
