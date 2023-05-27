b7df04aef3a3c1c6716a0803e5bc3f8561e6bc87
呉真 <kuretru@gmail.com>
2023-05-27 12:18:51 +0800

    新增(API)：百分比及金额格式转换

--- Libra-Fortune-Web/src/services/libra-fortune/ledger/ledger-entry.ts
+++ Libra-Fortune-Web/src/services/libra-fortune/ledger/ledger-entry.ts
@@ -8,6 +8,56 @@ class LedgerEntryService extends BaseService<
   constructor(ledgerId: string) {
     super(`/ledgers/${ledgerId}/entries`);
   }
+
+  fromServer(records: API.Ledger.LedgerEntryVO[]) {
+    records.forEach((record) => {
+      record.total /= 100;
+      record.details.forEach((detail) => {
+        detail.fundedRatio /= 100;
+        detail.amount /= 100;
+      });
+    });
+    return records;
+  }
+
+  toServer(record: API.Ledger.LedgerEntryDTO) {
+    record.total = Number.parseInt((record.total * 100).toFixed(0));
+    record.details.forEach((detail) => {
+      detail.fundedRatio = Number.parseInt((detail.fundedRatio * 100).toFixed(0));
+      detail.amount = Number.parseInt((detail.amount * 100).toFixed(0));
+    });
+    return record;
+  }
+
+  async list(
+    params?: API.Ledger.LedgerEntryQuery,
+  ): Promise<API.ApiResponse<API.Ledger.LedgerEntryVO[]>> {
+    return super.list(params).then((response) => {
+      response.data = this.fromServer(response.data);
+      return response;
+    });
+  }
+
+  async listByPage(
+    params: API.Ledger.LedgerEntryQuery & API.PaginationQuery,
+  ): Promise<API.ProTableData<API.Ledger.LedgerEntryVO>> {
+    return super.listByPage(params).then((response) => {
+      response.data = this.fromServer(response.data);
+      return response;
+    });
+  }
+
+  async create(
+    record: API.Ledger.LedgerEntryDTO,
+  ): Promise<API.ApiResponse<API.Ledger.LedgerEntryDTO>> {
+    return super.create(this.toServer(record));
+  }
+
+  async update(
+    record: API.Ledger.LedgerEntryDTO,
+  ): Promise<API.ApiResponse<API.Ledger.LedgerEntryDTO>> {
+    return super.update(this.toServer(record));
+  }
 }
 
 export default LedgerEntryService;
