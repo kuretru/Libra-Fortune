dd54ef7c94d917ca4e850cf68dbd201ab6c1777d
呉真 <kuretru@gmail.com>
2023-08-18 22:21:41 +0800

    更新(Web)：修改OAuth2的API路径

--- Libra-Fortune-Web/src/services/galaxy-oauth2-client/service.ts
+++ Libra-Fortune-Web/src/services/galaxy-oauth2-client/service.ts
@@ -3,7 +3,7 @@ import { request } from '@umijs/max';
 export async function galaxyAuthorize(
   record: Galaxy.OAuth2.Client.OAuth2AuthorizeRequestDTO,
 ): Promise<API.ApiResponse<string>> {
-  return request('/oauth2/galaxy/authorize', {
+  return request('/api/oauth2/galaxy/authorize', {
     method: 'POST',
     data: record,
   });
@@ -12,7 +12,7 @@ export async function galaxyAuthorize(
 export async function galaxyCallback(
   record: Galaxy.OAuth2.Client.OAuth2AuthorizeResponseDTO,
 ): Promise<API.ApiResponse<Galaxy.OAuth2.System.UserLoginDTO>> {
-  return request('/oauth2/galaxy/callback', {
+  return request('/api/oauth2/galaxy/callback', {
     method: 'POST',
     data: record,
   });
