b6cac4156fc0d0eafd5935d5cc3ccc5af3f8d4c6
呉真 <kuretru@gmail.com>
2026-07-11 14:11:23 +0800

    Feature(API): 增删改账户余额功能

--- Libra-Fortune-Api/libra-fortune-api/src/main/java/com/kuretru/web/libra/account/service/AccountBalanceService.java
+++ Libra-Fortune-Api/libra-fortune-api/src/main/java/com/kuretru/web/libra/account/service/AccountBalanceService.java
@@ -1,10 +1,13 @@
 package com.kuretru.web.libra.account.service;
 
+import com.kuretru.web.libra.account.entity.business.AccountBalanceRequest;
 import com.kuretru.web.libra.account.entity.business.AccountBalanceResultBO;
 import com.kuretru.web.libra.account.entity.query.AccountBalanceQuery;
 
 public interface AccountBalanceService {
 
     AccountBalanceResultBO list(AccountBalanceQuery query);
+
+    void save(AccountBalanceRequest request);
 
 }
