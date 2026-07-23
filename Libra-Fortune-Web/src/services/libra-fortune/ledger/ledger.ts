14004fe447baa1028bd2b93f84f5d6e616f1b5a1
呉真 <kuretru@gmail.com>
2026-07-23 21:37:58 +0800

    Delete(Web): V1代码下线

--- Libra-Fortune-Web/src/services/libra-fortune/ledger/ledger.ts
+++
@@ -1,9 +0,0 @@
-import BaseService from '@/services/galaxy-web/base-service';
-
-class LedgerService extends BaseService<API.Ledger.LedgerDTO, API.Ledger.LedgerQuery> {
-  constructor() {
-    super('/ledgers');
-  }
-}
-
-export default LedgerService;
