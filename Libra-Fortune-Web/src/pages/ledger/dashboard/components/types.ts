0e8fca5580baae851517d9b488d983533e16586b
呉真 <kuretru@gmail.com>
2026-08-01 16:46:25 +0800

    Feature(Web): 完善看板

---
+++ Libra-Fortune-Web/src/pages/ledger/dashboard/components/types.ts
@@ -0,0 +1,9 @@
+export type DashboardTimeRange = {
+  dateBegin: string;
+  dateEnd: string;
+};
+
+export type CategoryLabelMaps = {
+  categoryIdL1: Map<number, string>;
+  categoryIdL2: Map<number, string>;
+};
